50001662b34ff2674ff5cedf14eba34c67095dd0
Kevin Frantz <mail@kevin-frantz.de>
2017-04-17 19:06:41 +0000

    Metermessfunktion hinzugefuegt

--- skalieren.py
+++ skalieren.py
@@ -4,19 +4,25 @@ import time
 core=CORE()
 core.setSensorValues()
 core.printValues()
-print("Zum fortfahren beliebige Taste druecken...");
-getch.getch();
-core.turnRight();
-startTime=time.time();
-whileStatus=0
-while True:
-	if core.infarotMitteRechts.getValue():
-		if whileStatus==1:
-			runTime=time.time()-startTime;			
-			core.stop();
-			break;
-	else:
-		whileStatus=1;	
-print("Die ausfuehrzeit betraegt {0} Sekunden.".format(runTime));
+def trackTime():
+	
+	startTime=time.time();
+	whileStatus=0
+	while True:
+		if core.infarotMitteRechts.getValue():
+			if whileStatus==1:				
+				print("Die ausfuehrzeit betraegt {0} Sekunden.".format(time.time()-startTime));				
+				break;
+		else:
+			whileStatus=1;
+print("Auswaehlen: Sekunden pro Meter(m) oder Sekunden pro turn...");
+if(getch.getch()=='m'):
+	core.forward();
+	trackTime();
+	core.stop();
+else:
+	core.turnRight();
+	trackTime();
+	core.stop();
 			
 
